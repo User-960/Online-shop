bea4ce4e0fa4804e9da644b941da7bf78c9b44ef
Ivan <ivanvolodin960@gmail.com>
2023-03-21 20:40:23 +0300

    init: add framework vue 3

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,5 +1,8 @@
 const path = require('path');
+const Webpack = require('webpack');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 
 // UTILS
 const __base = path.resolve(__dirname, '..');
@@ -13,21 +16,37 @@ module.exports = {
 
   // GENERAL PLUGINS
   plugins: [
+    // to remove warn in browser console: runtime-core.esm-bundler.js:3607 
+    // Feature flags __VUE_OPTIONS_API__, __VUE_PROD_DEVTOOLS__ are not explicitly defined...
+    new Webpack.DefinePlugin({ 
+      __VUE_OPTIONS_API__: true, 
+      __VUE_PROD_DEVTOOLS__: true 
+    }),
     new HtmlWebpackPlugin({
       title: 'Minimal base config',
       favicon: path.resolve(__src, 'static', 'favicon.ico'),
       template: path.resolve(__src, 'templates', 'index.html'),
-    })
+    }),
+    new VueLoaderPlugin(),
   ],
 
    // GENERAL RULES
     module: {
       rules: [
+        // VUE FILES
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+        },
+
         // TYPESCRIPT FILES
         {
           test: /\.ts$/,
           loader: 'ts-loader',
-          exclude: /node_modules/
+          exclude: /node_modules/,
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+          },
         },
       ]
     },
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-alert("Mini Base Config...");
+import {createApp} from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
