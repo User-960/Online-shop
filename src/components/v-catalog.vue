68f429e13febe398067d422ca544d84de6fb83c0
Ivan <ivanvolodin960@gmail.com>
2023-03-27 01:00:33 +0300

    feat: add logic for getting data from server by API

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
     <h1>Catalog</h1>
     <div class="v-catalog__list">
       <vCatalogItem 
-        v-for="product in products"
+        v-for="product in PRODUCTS"
         :key="product.article"
         :product_data="product"
         @send-data-to-parent="showChildArticleInConsole"
@@ -14,6 +14,7 @@
 
 <script lang="ts">
   import { defineComponent } from 'vue';
+  import { mapGetters, mapActions } from 'vuex';
   import vCatalogItem from './v-catalog-item.vue';
 
   export default defineComponent({
@@ -23,63 +24,29 @@
     },
     props: {},
     data: () => ({
-      products: [
-        {
-          image: '1.jpg',
-          name: 'T-shirt 1',
-          price: 2100.234234234,
-          article: 'T1',
-          available: true,
-          category: 'Man'
-        },
-        {
-          image: '2.jpg',
-          name: 'T-shirt 2',
-          price: 3150.12312412,
-          article: 'T2',
-          available: true,
-          category: 'Woman'
-        },
-        {
-          image: '3.jpg',
-          name: 'T-shirt 3',
-          price: 4200.51524,
-          article: 'T3',
-          available: false,
-          category: 'Woman'
-        },
-        {
-          image: '4.jpg',
-          name: 'T-shirt 4',
-          price: 5300.1245512,
-          article: 'T4',
-          available: true,
-          category: 'Man'
-        },
-        {
-          image: '5.jpg',
-          name: 'T-shirt 5',
-          price: 6500.3522314,
-          article: 'T5',
-          available: false,
-          category: 'Woman'
-        },
-        {
-          image: '6.jpeg',
-          name: 'T-shirt 6',
-          price: 8700.4124123,
-          article: 'T6',
-          available: true,
-          category: 'Woman'
-        }
-      ]
+      
     }),
-    computed: {},
+    computed: {
+      ...mapGetters([
+        'PRODUCTS'
+      ])
+    },
+    mounted() {
+      this.GET_PRODUCTS_FROM_API()
+        .then((response) => {
+          if (response.data) {
+            console.log('Data arrived!');
+          }
+        });
+    },
     methods: {
+      ...mapActions([
+        'GET_PRODUCTS_FROM_API'
+      ]),
       showChildArticleInConsole(data: string) {
         console.log(data);
       }
-    }
+    },
   });
 </script>
 
--- src/store/modules/product.ts
+++ src/store/modules/product.ts
@@ -1,6 +1,32 @@
+import axios from 'axios';
+
 export default {
-  state: {},
-  actions: {},
-  mutations: {},
-  getters: {}
+  state: {
+    products: []
+  },
+  actions: {
+    async GET_PRODUCTS_FROM_API({commit}) {
+      return await axios('http://localhost:3001/products', {
+        method: 'GET',
+      })
+        .then((products) => {
+          commit('SET_PRODUCTS_TO_STATE', products.data);
+          return products;
+        })
+        .catch((error) => {
+          console.log(error);
+          return error;
+        });
+    }
+  },
+  mutations: {
+    SET_PRODUCTS_TO_STATE: (state, products) => {
+      state.products = products;
+    }
+  },
+  getters: {
+    PRODUCTS(state) {
+      return state.products;
+    }
+  }
 };
